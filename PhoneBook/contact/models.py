6548b5bde7db4257dc35a94b175f32c325e6f7bd
Raika <raika@Raik.TT>
2024-03-21 01:39:39 +0330

    Add user app, login page, register page, logout page,
    create base.html,
    
    create form.py and def views.sign_in, views.sign_up, views.sign_out.

--- PhoneBook/contact/models.py
+++ PhoneBook/contact/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        verbose_name="first name",
+    )
+    last_name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        verbose_name="last name",
+    )
+    number = models.CharField(
+        max_length=12,
+        blank=False,
+        null=False,
+        verbose_name="number",
+    )
+    address = models.TextField(
+        blank=False,
+        null=False,
+        verbose_name="address",
+    )
+
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- PhoneBook/contact/urls.py
+++ PhoneBook/contact/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import say_hello
 
 urlpatterns = [
-    path('', say_hello, name='hello'),
+    path('', say_hello, name='contacts'),
 ]
